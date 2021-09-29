ae03abedee9332abe756b91be0ff1cdd2967d882
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2021-09-29 15:19:40 -0400

    made minimum module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='TestModule',
+      version='1.0',
+      description='Mere sample setup.py',
+      author='S. S',
+      author_email='shiraiwa@princeton.edu',
+      package_dir = {'':'test_module'}
+     )
